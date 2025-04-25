28cbd09f15985e233821c5e050a784e00027fd42
Krishna Kant Sharma <kknewsits4u@gmail.com>
2025-04-25 13:01:00 +0530

    some changes

--- src/Components/ServiceCategory/ServiceCategories.jsx
+++ src/Components/ServiceCategory/ServiceCategories.jsx
@@ -94,7 +94,7 @@ const WeddingCategories = () => {
                 <h3 className="font-semibold  text-md text-slate-700 ">
                   {cat.title} <span className="text-sm">▾</span>
                 </h3>
-                <p className="text-sm  text-gray-600">{cat.description}</p>
+                <p className="text-[10px] md:text-[12px] lg:text-[15px]  text-gray-600">{cat.description}</p>
               </div>
               <img
                 src={cat.image}
@@ -108,7 +108,7 @@ const WeddingCategories = () => {
                 <div className="absolute top-[-5px] left-5 w-0 h-0 border-l-8 border-r-8 border-b-8 border-transparent border-b-white"></div>
 
                 <div className="bg-white  p-4 rounded   text-left shadow-md">
-                  <ul className="grid grid-cols-2 gap-2 text-slate-700 text-sm">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2 text-slate-700 text-sm">
                     {cat.subcategories.map((item, i) => (
                       <li
                         key={i}
